7fe51c9d299b852dc1a2a19b351d7ccb94eb23d3
Ry4n-Ferreir4 <130564393+Ry4n-Ferreir4@users.noreply.github.com>
2023-05-17 18:47:41 +0000

    Primeiro Commit

--- main.c
+++ main.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 
-#define N 3
+#define N 3 
 
 
-void calcularMatriz(int matrix1[][N], int matrix2[][N], int matrixsoma[][N]) {
+void calcularSoma(int n, int *matrix1, int *matrix2, int *matrixsoma) {
     int i, j;
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            matrixsoma[i][j] = matrix1[i][j] + matrix2[i][j];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            *(matrixsoma + i * n + j) = *(matrix1 + i * n + j) + *(matrix2 + i * n + j);
         }
     }
 }
 
-
-void exibirMatriz(int mat[][N]) {
+void exibirMatriz(int n, int *mat) {
     int i, j;
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            printf("%d ", mat[i][j]);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", *(mat + i * n + j));
         }
         printf("\n");
     }
 }
 
 int main() {
-    int matrix1[N][N] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int n = 3;
+   	int matrix1[N][N] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int matrix2[N][N] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
     int matrixsoma[N][N];
 
-    calcularMatriz(matrix1, matrix2, matrixsoma);
+   
+
+    calcularSoma(n, (int *)matrix1, (int *)matrix2, (int *)matrixsoma);
 
     printf("Matriz A:\n");
-    exibirMatriz(matrix1);
+    exibirMatriz(n, (int *)matrix1);
 
     printf("\nMatriz B:\n");
-    exibirMatriz(matrix2);
+    exibirMatriz(n, (int *)matrix2);
 
     printf("\nMatriz Soma C:\n");
-    exibirMatriz(matrixsoma);
+    exibirMatriz(n, (int *)matrixsoma);
 
     return 0;
 }
