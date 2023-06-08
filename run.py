587078d5a546706527a79eb527eb2b56d4bb4c4c
Ryan Mendes <130564393+Ry4n-Ferreir4@users.noreply.github.com>
2023-06-08 21:58:29 +0000

    06 - VERSION 1.0

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ nome_commit = str(input('Digite o nome do commit: '))
 
 os.system("git add .")
 os.system(f"git commit -m '{nome_commit}' ")
-os.system("git push -force")
+os.system("git push --force")
